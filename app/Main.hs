0543ac7f9ec5e1a8f316cf2f7c151462b7ce24b7
zitkat <zitkat@kky.zcu.cz>
2022-05-25 10:48:16 +0200

    Improve output with first renderer.

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,9 @@ module Main where
 
 import Inputs (userInputMeds)
 import Computations (computeUnitPrice)
+import Renders ( showProcessedMed )
 
 main :: IO ()
 main = do
-    meds <- userInputMeds []
-    print $ map computeUnitPrice meds
+    meds <- userInputMeds [] 
+    foldMap (putStr . showProcessedMed . computeUnitPrice) meds
--- src/Renders.hs
+++ src/Renders.hs
@@ -1,7 +1,14 @@
 module Renders where
 
+import Data
 import Computations (ProcessedMed, unpackP)
 
 showProcessedMed :: ProcessedMed -> String
-showProcessedMed m = show um 
+showProcessedMed m = "Jméno:    " ++ name um ++ "\n" ++
+                     "Forma:    " ++ show (form um) ++ "\n" ++
+                     "Jednotky: " ++ unit um ++ "\n" ++
+                     "Množství: " ++ show (total_amount um) ++ "\n" ++
+                     "Cena:     " ++ show (total_price um) ++ "\n" ++
+                     "Jdn. cena:" ++ show (unit_price um) ++ "\n\n"
         where um = unpackP m
+
